59e8a3e50bf0e3bc1bf5b5dcae0f9716f4b2b4c4
Cakey01 <alexanderwarren7@gmail.com>
2025-10-23 11:20:28 -0400

    Create list class to make list, add name and id, and add todos

--- src/js/list.js
+++ src/js/list.js
@@ -1,13 +1,14 @@
 import { Todo } from './todo.js';
 
 export class List {
-    constructor() {
-        this.list = [];
+    constructor(name) {
+        this.name = name;
+        this.id = crypto.randomUUID();
+        this.todos = [];
     }
 
     addTodo(title, description, dueDate, priority) {
         const todo = new Todo(title, description, dueDate, priority);
-
-        this.list.push(todo);
+        this.todos.push(todo);
     }
 }
