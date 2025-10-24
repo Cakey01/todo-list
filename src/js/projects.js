d6f6323039bd6f83e6f54a208eb70ef49e4d348c
Cakey01 <alexanderwarren7@gmail.com>
2025-10-24 11:17:24 -0400

    Add name check, duplicate check, and simplify active list of add List method

--- src/js/projects.js
+++ src/js/projects.js
@@ -7,12 +7,28 @@ export class Projects {
     }
 
     addList(name) {
+        // name check and trim
+        if (!name || !name.trim()) {
+            console.log(`no name or no trimmed name`);
+            return;
+        }
+
+        // no duplicates
+        const listNames = []
+        this.lists.forEach(list => {
+            listNames.push(list.name);
+        });
+
+        if (listNames.includes(name.trim())) {
+            return;
+        }
+
         // create list and push
-        const list = new List(name);
+        const list = new List(name.trim());
         this.lists.push(list);
         
-        if (!this.active != list) {
-            this.active = list;
-        }
+        // make active list
+        this.active = list;
     }
+
 }
